8138632e434bbcee8fc92f3c17bb782164021b05
koushikalakhamraju <lakhamrajukoushika@gmail.com>
2022-02-05 11:19:10 +0530

    my assignment-2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -4,13 +4,23 @@ import './App.css';
 
 
 function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+  const { employee } = props;
+
+  return (
+    <div className="container">
+      {/* Start your React code here */}
+      <div className="employeeDetails">
+        <img className="ProfilePic" src={employee.profileImg} alt="give proper adress" />
+        <p className="employeeName">{employee.name}</p>
+        <label className="locationDetails">Location</label>
+        <b>{employee.location}</b>
+        <label className="bloodgroupDetails">BloodGroup</label>
+        <b>{employee.bloodGroup}</b>
+        <label className="ageDetails">Age</label>
+        <b>{employee.age}</b>
+      </div>
+    </div>
+  )
 }
 
 
